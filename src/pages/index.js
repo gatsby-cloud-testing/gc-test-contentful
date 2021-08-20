8d3564ad47438123d58dd7e7fd58a5d41b88575e
gdhardy1 <47133442+gdhardy1@users.noreply.github.com>
2021-08-20 11:13:04 -0400

    don't query tags

--- src/pages/index.js
+++ src/pages/index.js
@@ -48,7 +48,6 @@ export const pageQuery = graphql`
           title
           slug
           publishDate(formatString: "MMMM Do, YYYY")
-          tags
           heroImage {
             fluid(maxWidth: 350, maxHeight: 196, resizingBehavior: SCALE) {
               ...GatsbyContentfulFluid_tracedSVG
